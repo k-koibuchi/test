f0311c727c51a19b6c5afc4a093bbdad4b08a6ca
k-koibuchi <k-koibuchi@shotoku.co.jp>
2020-03-06 09:19:15 +0900

    add function2

--- testsorce/Source.cpp
+++ testsorce/Source.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	std::cout << "Hello World" << std::endl;
 	std::cout << "add function1 << std::endl;
+	std::cout << "add function2 << std::endl;
 	std::cout << "I—¹‚·‚é‚É‚ÍEnter‚ğ“ü—Í" << std::endl;
 	getchar();
 
