68a4969e834b93cd428937c5fe441b9a85be6d57
k-koibuchi <k-koibuchi@shotoku.co.jp>
2020-03-11 13:16:28 +0900

    add function4

--- testsorce/Source.cpp
+++ testsorce/Source.cpp
@@ -6,6 +6,7 @@ int main()
 	std::cout << "add function1 << std::endl;
 	std::cout << "add function2 << std::endl;
 	std::cout << "add function3 << std::endl;
+	std::cout << "add function4 << std::endl;
 	std::cout << "終了するにはEnterを入力" << std::endl;
 	getchar();
 
